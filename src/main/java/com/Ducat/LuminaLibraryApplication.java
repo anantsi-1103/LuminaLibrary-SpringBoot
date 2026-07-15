7dfcf30c60300e52d9cd868cec5aa46c1fba7a3d
anantsi-1103 <acera52024@gmail.com>
2026-07-15 17:58:40 +0530

    library

---
+++ src/main/java/com/Ducat/LuminaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.Ducat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminaLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ducat/LuminaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ducat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
